2c0fbd4ed2d333152e2c748a6229bf026fa005ff
maikenAR <maiken@artificialrome.com>
2025-06-18 12:41:17 +0200

    Add default image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,10 +6,11 @@ import { Link } from 'react-router-dom'
 export default function Card({title,description,page}) {
     return (
         <div className="card mb-4 shadow-sm" style={{ width: '22rem' }}>
-            <img src="..." class="card-img-top" alt="..."/>
+            <img className="img-fluid" src="/MockUpSmartphone.jpg" class="card-img-top" alt="..."/>
                 <div class="card-body text-start">
+                    <p class="card-text economica-regular fs-3 ">{title}</p>
                     <p class="card-text lato-regular  ">{description}</p>
-                    <Link to={page}class="btn btn-outline-secondary">Go somewhere</Link>
+                    <Link to={page}class="btn btn-outline-secondary">View project</Link>
                 </div>
         </div>
     )
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,9 +4,9 @@ import './Layout.css'
 export default function Layout({ children }) {
   return (
     <div className="row image-gradient-background">
-      <div class={`col-3`}></div>
-      <div class={`col-6`}>{children}</div>
-      <div class={`col-3`}></div>
+      <div class={`col-4`}></div>
+      <div class={`col-4`}>{children}</div>
+      <div class={`col-4`}></div>
     </div>
   )
 }
